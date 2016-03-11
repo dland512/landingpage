c5dafe5c1fdb579abb2e238a674534183ad3126f
David Land <david.k.land@gmail.com>
2016-03-11 15:07:52 -0600

    more design stuff

--- config/ignite.js
+++ config/ignite.js
@@ -2,7 +2,7 @@ module.exports = {
     domain: 'http://www.ignitesolutions.software',
     port: 3000,
     name: 'Ignite Software',
-    logo: 'logo-ignite.png',
+    logo: 'logo-ignite-w.png',
     contact: {
         to: 'david.k.land@gmail.com',
         username: 'testuser.ignite@gmail.com',
